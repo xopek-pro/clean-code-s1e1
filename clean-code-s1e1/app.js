5b612a10b62cb2eba04605b5f0cd88134a4e5eaa
xopek-pro <xopek.pro@internet.ru>
2021-04-08 19:27:02 +0300

    refactor: use BEM notation for header and first section (extended rule 2.1 in html-and-css-extended.md)

--- clean-code-s1e1/app.js
+++ clean-code-s1e1/app.js
@@ -9,7 +9,8 @@
 // Event handling, user interaction is what starts the code execution.
 
 var taskInput=document.getElementById("new-task");//Add a new task.
-var addButton=document.getElementsByTagName("button")[0];//first button
+//var addButton=document.getElementsByTagName("button")[0];//first button
+var addButton=document.querySelector("#add-button");//first button
 var incompleteTaskHolder=document.getElementById("incomplete-tasks");//ul of #incomplete-tasks
 var completedTasksHolder=document.getElementById("completed-tasks");//completed-tasks
 
